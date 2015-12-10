da52b2bc10b64579099b8618410c02ca5a7ecc0f
jeroentempels <jeroen.tempels@student.kuleuven.be>
2015-12-10 12:18:57 +0100

    added comments and removed unused code

--- src/solver/GameState.java
+++ src/solver/GameState.java
@@ -1,14 +1,23 @@
 package solver;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
+/**
+ * Class that represents the state of the game at a certain point in time.
+ */
 public class GameState {
 
+    // Amount of moves performed since initial state.
     private final int movesPerformed;
+
+    // The current state of the board;
     private final Board board;
+
+    // The previous state the game was in.
     private final GameState previous;
+
+    // The move performed in the previous state to get to this state.
     private final Direction move;
 
     private GameState(int movesPerformed, Board board, GameState previous, Direction move) {
@@ -18,48 +27,70 @@ public class GameState {
         this.previous = previous;
     }
 
+    /**
+     * Create a initial game state.
+     *
+     * @param board The initial board.
+     */
     GameState(Board board) {
         this(0, board, null, null);
     }
 
+    /**
+     * @return The amount of moves performed to get to this state from the initial state.
+     */
     public int getMovesPerformed() {
         return movesPerformed;
     }
 
+    /**
+     * @return The board belonging to this state.
+     */
     public Board getBoard() {
         return board;
     }
 
+    /**
+     * @return The move performed in the previous state to get to this state.
+     */
     public Direction getMove() {
         return move;
     }
 
+    /**
+     * @return The state the game was in before this one.
+     */
+    public GameState getPrevious() {
+        return previous;
+    }
+
+
+    /**
+     * Check whether this state is an end state.
+     *
+     * @param row The goal row for the bird.
+     * @param column The goal column for the bird.
+     * @param dir The goal direction for the bird.
+     *
+     * @return True if this state is a end state. False otherwise.
+     */
+    public boolean isFinished(int row, int column, Direction dir) {
+        return board.isFinished(row, column, dir);
+    }
+
+    /**
+     * Calculate the states following this one.
+     *
+     * @return The set of all states that follow this state.
+     */
     Set<GameState> getNextStates() {
         Set<GameState> nextStates = new HashSet<>();
         for (Direction dir : Direction.values()) {
-            if (dir == move) {
+            if (dir == move) { // optimization
                 continue;
             }
             nextStates.add(new GameState(movesPerformed + 1, board.copyAndMove(dir), this, dir));
         }
         return nextStates;
-    }
-
-    @Override
-    public String toString() {
-        return "GameState{" +
-                "movesPerformed=" + movesPerformed +
-                ", board=" + board +
-                ", move=" + move +
-                '}';
-    }
-
-    public boolean isFinished(int x, int y, Direction dir) {
-        return board.isFinished(x, y, dir);
-    }
-
-
-    public GameState getPrevious() {
-        return previous;
     }
 }
