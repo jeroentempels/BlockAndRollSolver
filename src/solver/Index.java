9b490b04366f068761a8089867842c5dfa2c3bf9
jeroentempels <jeroen.tempels@student.kuleuven.be>
2015-12-10 13:29:04 +0100

    added comments

--- src/solver/Index.java
+++ src/solver/Index.java
@@ -1,32 +1,59 @@
 package solver;
 
 /**
- * Created by jeroen on 9/12/15.
+ * Class representing an index of a 2d-array.
  */
 public class Index {
 
+    // row index
     private final int first;
+
+    // column index
     private final int second;
 
+    /**
+     * Create a new index.
+     *
+     * @param first  The row index of the index.
+     * @param second The column index of the index.
+     */
     public Index(int first, int second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * @return The row index.
+     */
     public int getFirst() {
         return first;
     }
 
+    /**
+     * @return The column index.
+     */
     public int getSecond() {
         return second;
     }
 
+    /**
+     * Add a index to this index.
+     *
+     * @param index The index to add.
+     * @return The index that is the result of adding this index to the given one.
+     */
     public Index plus(Index index) {
-        return new Index(first+index.first, second+index.second);
+        return new Index(first + index.first, second + index.second);
     }
 
+    /**
+     * Subtract an index from this index.
+     *
+     * @param index The index to subtract.
+     * @return The index that is the result of subtracting the given index from this one.
+     */
     public Index min(Index index) {
-        return new Index(first-index.first, second-index.second);
+        return new Index(first - index.first, second - index.second);
     }
 
     @Override
