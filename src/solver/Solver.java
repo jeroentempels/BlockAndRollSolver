22208b723948e9ae0f9505902853b71ad8606029
jeroentempels <jeroen.tempels@student.kuleuven.be>
2015-12-10 16:21:56 +0100

    removed caching hack, replaced by caching of blocks

--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -7,26 +7,21 @@ import java.util.*;
 
 public class Solver {
 
-    private static final Block dummy = new Block(new Index(-1,-1),true, true, true, true);
-
     public static List<Direction> solve(Board board, int x, int y, Direction dir) {
 
         Queue<GameState> states = new LinkedList<>();
         states.add(new GameState(board));
-        Set<Set<Block>> cache = new HashSet<>();
+        Set<Board> cache = new HashSet<>();
 
         while(!states.peek().isFinished(x,y,dir)) {
             for(GameState state : states.poll().getNextStates()) {
-                Set<Block> c = state.getBoard().getBlocks();
-                c.add(new Block(state.getBoard().getBird(), dummy));
-                if(cache.contains(c)) {
+                if(cache.contains(state.getBoard())) {
                     continue;
                 }
-                cache.add(c);
+                cache.add(state.getBoard());
                 states.add(state);
             }
         }
-
         return getMoves(states.poll());
     }
 
