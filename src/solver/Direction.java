eccd6c2d20a1b9c6a5d8961830f56bffb21d4914
jeroentempels <jeroen.tempels@student.kuleuven.be>
2015-12-10 13:25:51 +0100

    added comments

--- src/solver/Direction.java
+++ src/solver/Direction.java
@@ -7,54 +7,71 @@ import java.util.stream.Collectors;
 public enum Direction {
     UP {
         @Override
-        public Index getIndex() {
+        Index getIndex() {
             return new Index(-1, 0);
         }
 
         @Override
-        public List<Block> sort(Collection<Block> blocks) {
+        List<Block> sort(Collection<Block> blocks) {
             Comparator<Block> comp = (Block b1, Block b2) -> Integer.compare(b1.getIndex().getFirst(), b2.getIndex().getFirst());
             return sort(blocks, comp);
         }
     }, DOWN {
         @Override
-        public Index getIndex() {
+        Index getIndex() {
             return new Index(1, 0);
         }
 
         @Override
-        public List<Block> sort(Collection<Block> blocks) {
+        List<Block> sort(Collection<Block> blocks) {
             Comparator<Block> comp = (Block b1, Block b2) -> Integer.compare(-b1.getIndex().getFirst(), -b2.getIndex().getFirst());
             return sort(blocks, comp);
         }
     }, LEFT {
         @Override
-        public Index getIndex() {
+        Index getIndex() {
             return new Index(0, -1);
         }
 
         @Override
-        public List<Block> sort(Collection<Block> blocks) {
+        List<Block> sort(Collection<Block> blocks) {
             Comparator<Block> comp = (Block b1, Block b2) -> Integer.compare(b1.getIndex().getSecond(), b2.getIndex().getSecond());
             return sort(blocks, comp);
         }
     }, RIGHT {
         @Override
-        public Index getIndex() {
+        Index getIndex() {
             return new Index(0, 1);
         }
 
         @Override
-        public List<Block> sort(Collection<Block> blocks) {
+        List<Block> sort(Collection<Block> blocks) {
             Comparator<Block> comp = (Block b1, Block b2) -> Integer.compare(-b1.getIndex().getSecond(), -b2.getIndex().getSecond());
             return sort(blocks, comp);
         }
     };
 
-    public abstract Index getIndex();
+    /**
+     * @return Index representing the movement in this direction.
+     */
+    abstract Index getIndex();
 
-    public abstract List<Block> sort(Collection<Block> blocks);
 
+    /**
+     * Sort block according to this direction.
+     *
+     * @param blocks The blocks to sort.
+     * @return A sorted list of blocks.
+     */
+    abstract List<Block> sort(Collection<Block> blocks);
+
+    /**
+     * Sort blocks with given comparator.
+     *
+     * @param blocks          The blocks to sort.
+     * @param blockComparator The comparator to use while sorting.
+     * @return A sorted list of the given blocks.
+     */
     List<Block> sort(Collection<Block> blocks, Comparator<Block> blockComparator) {
         return blocks.stream().sorted(blockComparator).collect(Collectors.toList());
     }
