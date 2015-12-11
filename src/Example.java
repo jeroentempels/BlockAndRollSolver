39e9c0aaabb53b9f13e0b2e1fb7cf92e68ee3c73
jeroentempels <jeroen.tempels@student.kuleuven.be>
2015-12-11 02:34:03 +0100

    bigger example than in readme

--- src/Example.java
+++ src/Example.java
@@ -7,19 +7,24 @@ public class Example {
     public static void main(String[] args) {
 
         boolean[][] field = {
-                {true, false, false},
-                {true, true, true},
-                {false, false, true}
+                {true, false, true, true, false},
+                {true, true, true, true, true},
+                {true, true, true, true, true},
+                {true, true, true, false, false},
+                {true, true, true, true, false}
         };
 
         BoardBuilder builder = new BoardBuilder(field);
 
-        builder.addBlock(0, 0, true, true, false, true);
-        builder.addBlock(1, 2, false, true, false, true);
+        builder.addBlock(0, 2, true, false, true, true);
+        builder.addBlock(1, 1, true, true, true, false);
+        builder.addBlock(2, 0, true, false, true, false);
+        builder.addBlock(3, 0, true, true, false, true);
+        builder.addBlock(3, 1, false, false, true, true);
 
-        builder.addBird(0, 0);
+        builder.addBird(0, 2);
 
-        Solver.solve(builder.toBoard(), 2, 2, Direction.DOWN).forEach(System.out::println);
+        Solver.solve(builder.toBoard(), 4, 2, Direction.DOWN).forEach(System.out::println);
 
     }
 
